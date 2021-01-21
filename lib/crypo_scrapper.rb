0a8145f4243ee1acf67395d8f13795f2c32ae6c9
volpito <mpj.rigot@gmail.com>
2021-01-21 11:58:13 +0100

    scrapin v2

--- lib/crypo_scrapper.rb
+++ lib/crypo_scrapper.rb
@@ -3,16 +3,28 @@ require 'open-uri'
 
 # Fetch and parse HTML document
 def crypto_scrapper
-  doc = Nokogiri::HTML(URI.open('https://coinmarketcap.com/all/views/all/'))
-  x = 0
+  @doc = Nokogiri::HTML(URI.open('https://coinmarketcap.com/all/views/all/'))
 # Search for nodes by xpath
+end
+
+def xpath
+	x = 0
 	while x < 100
-	doc.xpath("/html/body/div/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[#{x}]/td[2]/div/a").each do |link|
- 	  puts link.content
- 	end 
+	@doc.xpath("//html/body/div/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[#{x}]/td[3]").each do |name|
+	#puts name.content
+	@doc.xpath("/html/body/div/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[#{x}]/td[5]/div").each do |price|
+	#price = price.to_sym
+	#puts price.content
+	#puts name.content => price.content
+	array = name.content << (" => #{price.content}")
+	puts array
+end
+end 
   x += 1
+
 end
 end
+
 # Or mix and match
 #doc.search('nav ul.currencies li a', '//article//h2').each do |link|
   #puts link.content
@@ -26,6 +38,7 @@ end
 
 def perform
 crypto_scrapper
+xpath
 end
 
 perform
