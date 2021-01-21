2a104cd066cb3153814b5c56988af745e9323982
anthopayan <76994877+anthopayan@users.noreply.github.com>
2021-01-22 00:08:23 +0100

    Update crypto_scrapper.rb

--- lib/crypto_scrapper.rb
+++ lib/crypto_scrapper.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'nokogiri'
 require 'open-uri'
 
@@ -7,24 +5,24 @@ require 'open-uri'
 def crypto_scrapper
   doc = Nokogiri::HTML(URI.open('https://coinmarketcap.com/all/views/all/'))
   x = 0
-  liste = []
-  # Search for nodes by xpath
-  while x <= 200
+  liste = Array.new
+# Search for nodes by xpath
+  while x <=200
     doc.xpath("/html/body/div/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[#{x}]/td[3]").each do |link|
-      doc.xpath("/html/body/div/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[#{x}]/td[5]/div/a").each do |price|
-        array = "\{ \"#{link.content}\" => " << "#{price.content} \}, "
-        liste.push(array)
-      end
-    end
+       doc.xpath("/html/body/div/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[#{x}]/td[5]/div/a").each do |price|
+          array = "\{ \"#{link.content}\" => " << "#{price.content} \}, "
+          liste.push(array)
+        end 
+     end 
     x += 1
   end
-  liste
+   return liste
 end
 
 def perform
-  crypto_scrapper
+crypto_scrapper
 end
 
-puts 'Voici la liste: '
-puts ''
+puts "Voici la liste: "
+puts ""
 puts perform
