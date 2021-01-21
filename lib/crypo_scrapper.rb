ed3b417f71fe439a3d0dfd7ee5b5cccf7c436ef2
volpito <mpj.rigot@gmail.com>
2021-01-21 12:16:50 +0100

    scrap_crypto_final_1

--- lib/crypo_scrapper.rb
+++ lib/crypo_scrapper.rb
@@ -11,11 +11,9 @@ def xpath
 	x = 0
 	while x < 100
 	@doc.xpath("//html/body/div/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[#{x}]/td[3]").each do |name|
-	#puts name.content
+
 	@doc.xpath("/html/body/div/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[#{x}]/td[5]/div").each do |price|
-	#price = price.to_sym
-	#puts price.content
-	#puts name.content => price.content
+	
 	array = ("\{ \"name.content\"") << (" => #{price.content} \}, ")
 	array_fin = Hash.new
 	array_fin = array
@@ -27,16 +25,6 @@ end
 end
 end
 
-# Or mix and match
-#doc.search('nav ul.currencies li a', '//article//h2').each do |link|
-  #puts link.content
-#end
-#end
-
-#def arr
-#	@crypto_scrapper
-#	link.  .each do |result|
-#end
 
 def perform
 crypto_scrapper
