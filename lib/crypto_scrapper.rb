d8f8480a0bcbd1301907fdad012e19f388ab7717
volpito <mpj.rigot@gmail.com>
2021-01-21 12:28:38 +0100

    LE SCRAPPER DE LA FIN

--- lib/crypto_scrapper.rb
+++ lib/crypto_scrapper.rb
@@ -9,7 +9,7 @@ def crypto_scrapper
 	while x < 100
   doc.xpath("/html/body/div/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[#{x}]/td[3]").each do |link|
          doc.xpath("/html/body/div/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[#{x}]/td[5]/div/a").each do |price|
-          array = "\"#{link.content}\" => " << price.content
+          array = "\{ \"#{link.content}\" => " << "#{price.content} \}, "
           puts array
           
     end 
