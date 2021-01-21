53418f2b5783129697e46bdd3f0b1c8be0fdc5e2
anthopayan <anthony.payan08@outlook.fr>
2021-01-21 21:24:29 +0100

    Fini

--- lib/crypto_scrapper.rb
+++ lib/crypto_scrapper.rb
@@ -5,21 +5,24 @@ require 'open-uri'
 def crypto_scrapper
   doc = Nokogiri::HTML(URI.open('https://coinmarketcap.com/all/views/all/'))
   x = 0
+  liste = Array.new
 # Search for nodes by xpath
-	while x < 200
-  doc.xpath("/html/body/div/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[#{x}]/td[3]").each do |link|
-         doc.xpath("/html/body/div/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[#{x}]/td[5]/div/a").each do |price|
-          array = [link.content => "#{price.content}"]
-          puts array
-    end 
- 	end 
+	while x <=200
+    doc.xpath("/html/body/div/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[#{x}]/td[3]").each do |link|
+       doc.xpath("/html/body/div/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[#{x}]/td[5]/div/a").each do |price|
+          array = "\{ \"#{link.content}\" => " << "#{price.content} \}, "
+          liste.push(array)
+        end 
+     end 
     x += 1
   end
+   return liste
 end
 
 def perform
 crypto_scrapper
 end
 
-
-perform
+puts "Voici la liste: "
+puts ""
+puts perform
